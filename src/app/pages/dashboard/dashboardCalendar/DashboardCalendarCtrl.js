538e5a3ec1b882c8ab6b89d0f488d22b0d842748
TheAlgo <dhirajkumar.jain2016@vitstudent.ac.in>
2018-03-25 22:42:13 +0530

    fgfg

---
+++ src/app/pages/dashboard/dashboardCalendar/DashboardCalendarCtrl.js
@@ -0,0 +1,48 @@
+/**
+ * @author v.lugovksy
+ * created on 16.12.2015
+ */
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.dashboard')
+      .controller('DashboardCalendarCtrl', DashboardCalendarCtrl);
+
+  /** @ngInject */
+  function DashboardCalendarCtrl(baConfig) {
+    var dashboardColors = baConfig.colors.dashboard;
+    var $element = $('#calendar').fullCalendar({
+      //height: 335,
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay'
+      },
+      defaultDate: '2016-03-08',
+      selectable: true,
+      selectHelper: true,
+      select: function (start, end) {
+        var title = prompt('Event Title:');
+        var eventData;
+        if (title) {
+          eventData = {
+            title: title,
+            start: start,
+            end: end
+          };
+          $element.fullCalendar('renderEvent', eventData, true); // stick? = true
+        }
+        $element.fullCalendar('unselect');
+      },
+      editable: true,
+      eventLimit: true, // allow "more" link when too many events
+      events: [
+        {
+          title: 'All Day Event',
+          start: '2016-03-01',
+          color: dashboardColors.silverTree
+        }
+      ]
+    });
+  }
+})();
